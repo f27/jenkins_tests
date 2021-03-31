1f46c254fc574c8d516aa67be5f685c050905da7
John Smoker <testgits@yandex.ru>
2021-03-31 16:02:06 +0500

    1st commit

---
+++ src/test/java/SimpleTests.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleTests {
+    @Test
+    void firstTest() {
+        assertEquals(true, true);
+    }
+}
